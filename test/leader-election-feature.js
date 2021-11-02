1ecd9ddd0f4d0376f0c0452c676aa37b1407c24d
Pål Edman <pal.edman@gmail.com>
2021-11-02 12:55:38 +0100

    bump all dependencies

--- test/leader-election-feature.js
+++ test/leader-election-feature.js
@@ -3,37 +3,56 @@
 // NOTE - requires consul started on localhost:8500 in dev mode:
 // consul agent -dev -advertise=127.0.0.1
 
-var leaderElection = require("../");
-var prefix = "exp-leader-election-feature-test-";
-var testKey = prefix + new Date().getTime();
-var config = {key: testKey, consul: {ttl: 10, lockDelay: 0, readWait: 180}};
-var consul = require("consul")();
+const leaderElection = require("../");
+const prefix = "exp-leader-election-feature-test-";
+const testKey = prefix + new Date().getTime();
+const Consul = require("consul");
 
-Feature("Elect leader", function () {
+const options = {
+  host: "localhost",
+  port: process.env.CONSUL_PORT,
+};
 
-  before(function (done) {
-    consul.kv.delete({key: prefix, recurse: true}, done);
+const config = {
+  key: testKey,
+  consul: {
+    ttl: 10,
+    lockDelay: 0,
+    readWait: 180,
+    ...options,
+  },
+};
+
+Feature("Elect leader", () => {
+  let consul;
+  before((done) => {
+    consul = Consul(options);
+    return consul.kv.delete({key: prefix, recurse: true}, done);
   });
 
-  Scenario("Two clients", function () {
-    var client, client2;
-    var sessionId;
-    When("First client is connected to consul it should be leader", function (done) {
+  Scenario("Two clients", () => {
+    let sessionId;
+    after((done) => {
+      return consul.session.destroy(sessionId, done);
+    });
+
+    let client, client2;
+    When("First client is connected to consul it should be leader", (done) => {
       client = leaderElection(config);
-      client.on("gainedLeadership", function (pSessionId) {
+      client.on("gainedLeadership", (pSessionId) => {
         sessionId = pSessionId;
         done();
       });
     });
-    When("Second client is connected", function () {
+    When("Second client is connected", () => {
       client2 = leaderElection(config);
     });
-    Then("It becomes leader if the first client is disconnected", function (done) {
-      client2.on("gainedLeadership", function () {
+    Then("It becomes leader if the first client is disconnected", (done) => {
+      client2.on("gainedLeadership", () => {
         done();
       });
-      client.on("error", function () {});
-      consul.session.destroy(sessionId, function () {});
+      client.on("error", () => {});
+      consul.session.destroy(sessionId, () => {});
     });
   });
 });
